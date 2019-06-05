437dbe104b936c24d98448182fc4adeee1325dcb
insung3511 <insung.park123@gmail.com>
2019-06-05 10:40:56 +0900

    Done :)
    숙제 다했어요 :+1:

--- ExtraHW/파일 입출력/Star.c
+++ ExtraHW/파일 입출력/Star.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main() {
-
+    FILE *fp = fopen("a.txt", "w");
+    for(int i = 0; i < 5 / 2 + 1; i++) {
+        for (int j = 0; j < 5 / 2 - i; j++) {
+            fputs(" ", fp);
+        }
+        for (int j = 0; j < i * 2 + 1; j++) {
+            fputs("*", fp);
+        }   fputs("\n", fp);
+    }
+    return 0;
 }
